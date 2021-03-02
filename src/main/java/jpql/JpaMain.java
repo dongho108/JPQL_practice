c8498c0760759d34bbbac842c88551ed2ef064e5
dongho <dongho108@naver.com>
2021-03-02 13:26:10 +0900

    add jpql fetch join etc..

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -11,27 +12,43 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setAge(0);
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setAge(0);
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("관리자3");
+            member3.setAge(0);
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
 
-            //language=HQL
-            String query = "select function('group_concat', m.username) from Member m ";
+            em.clear();
 
-            List<String> result = em.createQuery(query, String.class)
-                    .getResultList();
+            Member member = em.find(Member.class, member1.getId());
+            System.out.println("member.getAge() = " + member.getAge());
 
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
 
             tx.commit();
         } catch (Exception e) {
